713f7643fc2e6f0e9d3865a61691813243e5b2ba
Marcelo Magalhães <marcelo.magalhaes13@etec.sp.gov.br>
2020-06-04 17:54:12 -0300

    Index da aplicação

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="style.css" media="screen"/>
+</head>
+<body>
+    <div class="container">
+
+        <div class="box">
+            <h1 class="text1">Binário para decimal</h1>
+
+            <form method="post" action="page2.php" class="text2">
+                <p>Informe o valor em binário:</p>
+                <input type="number" name="bin"/><br>
+                <input class="botao" type="submit" value="Converter"/>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
