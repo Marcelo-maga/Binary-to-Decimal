fffae97736bff0e3b43702b577f03c586e61e5c6
Marcelo Magalhães <marcelo.magalhaes13@etec.sp.gov.br>
2020-06-04 17:56:38 -0300

    Página com códico PHP

---
+++ page2.php
@@ -0,0 +1,26 @@
+<?php
+    $c = bindec($_POST['bin']);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="style.css" media="screen"/>
+</head>
+<body>
+    <div class="container">
+
+        <div class="box">
+            <h1 class="text1">Binário para decimal</h1>
+
+            <form method="post" action="page2.php" class="text2">
+                <p>Informe o valor em binário:</p>
+                <input type="number" name="bin"/><br>
+                <input class="botao" type="submit" value="Converter"/>
+            </form>
+                <p class="text2">Decimal: <?php echo $c;?></p>
+        </div>
+    </div>
+</body>
+</html>
